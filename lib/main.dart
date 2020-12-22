5796bbd018e670e628ce6a08da0b4e9fd96443b4
bmdesai0309 <bmdesai1685@gmail.com>
2020-12-22 15:01:55 +0530

    set up question class and update list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/questions.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,10 +29,12 @@ class _QuizPageState extends State<QuizPage> {
 
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+
+  List<Questions> questionBank = [
+    Questions(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Questions(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Questions(q: 'A slug\'s blood is green.', a: true),
+
   ];
 
   int questionNumber = 0;
@@ -48,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,6 +76,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool currectAnswer = questionBank[questionNumber].questionAnswer;
+
+                if(currectAnswer == true) {
+                  print('User got it right');
+                }else {
+                  print('user got it wrong');
+                }
+
+
                 setState(() {
                   questionNumber++;
                 });
@@ -94,6 +106,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool currectAnswer = questionBank[questionNumber].questionAnswer;
+
+                if(currectAnswer == false) {
+                  print('User got it right');
+                }else {
+                  print('user got it wrong');
+                }
+
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
---
+++ lib/questions.dart
@@ -0,0 +1,13 @@
+class Questions {
+
+  String questionText;
+  bool questionAnswer;
+
+  Questions({String q, bool a}) {
+    questionAnswer = a;
+    questionText = q;
+  }
+
+
+
+}
